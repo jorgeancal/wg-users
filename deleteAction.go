20a6fcc64e086f0cc2e5e359d5edf9a0f39193cf
Jorge Andreu Calatayud <jorge.calatayud@crunch.co.uk>
2021-07-28 15:30:05 +0100

    JAC - Creating Delete Action

--- deleteAction.go
+++ deleteAction.go
@@ -1,9 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 /**
  * Delete Users
  * we will execute the next command `wg set wg0 peer $pubKey remove
  */
 func deleteUsers(users []string) {
-
+	currentUsers := getUsersList()
+	for _, currentUser := range currentUsers {
+		for _, user := range users {
+			if currentUser.name == user {
+				var command = "wg set wg0 peer " + currentUser.publicKey + " remove"
+				cmd := exec.Command("bash", "-c", command)
+				_, errO := cmd.Output()
+				if errO != nil {
+					fmt.Printf("There was an error delete %s user \n", currentUser.name)
+					os.Exit(-1)
+				}
+			}
+		}
+	}
 }
