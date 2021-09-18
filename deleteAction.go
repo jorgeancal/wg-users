a15b1279a8328dabce83b22eb17018d486284de4
Jorge Andreu Calatayud <info@jorgeancal.com>
2021-09-18 19:31:32 +0100

    JAC - First Draft about the copying the Config File.

--- deleteAction.go
+++ deleteAction.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 )
@@ -31,5 +32,32 @@ func deleteUsers(users []string) {
 
 // we are going to create a wg0-new.conf then mv the old one to the new one.
 func removeLineInCSV(users []string) {
+	copyConfigFile()
+}
 
+func copyConfigFile() {
+	sourceFileStat, err := os.Stat(FILES[1])
+	if err != nil {
+		fmt.Errorf("Error trying to get %s FileInf\n", FILES[1])
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		fmt.Errorf("%s is not a regular file\n", FILES[1])
+	}
+
+	source, err := os.Open(FILES[1])
+	if err != nil {
+		fmt.Errorf("%s failed to open\n", FILES[1])
+	}
+	defer source.Close()
+
+	destination, err := os.Create(DIRS[1] + "wg0-new.conf")
+	if err != nil {
+		fmt.Errorf("%s failed to open\n", DIRS[1]+"wg0-new.conf")
+	}
+	defer destination.Close()
+	_, err = io.Copy(destination, source)
+	if err != nil {
+		fmt.Errorf("%s failed to copy\n", DIRS[1]+"wg0-new.conf")
+	}
 }
